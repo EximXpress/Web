eebb12bbc1e1e7b5ff065e99fbc8cf531b6d98bd
EximXpress <info.eximxpress@gmail.com>
2025-05-01 23:57:49 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+
 // Middleware
 app.use(cors({ origin: 'https://www.eximxpress.com' })); // Allow CORS from your frontend domain
 app.use(express.json()); // Parse JSON bodies
